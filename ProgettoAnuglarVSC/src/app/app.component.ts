990c82821c642877fe6dafc5c9d128c4e077867b
reddooneee <daniele.coppa30@gmail.com>
2025-01-08 16:36:18 +0100

    Creazione Componente Home + Inizio HomeLocation

--- ProgettoAnuglarVSC/src/app/app.component.ts
+++ ProgettoAnuglarVSC/src/app/app.component.ts
@@ -1,11 +1,23 @@
+//import { RouterOutlet } from '@angular/router'; Da Togliere il commento se serve "RouterOutlet", "AppProvaComponent"
+//import { AppProvaComponent } from './app-prova/app-prova.component';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [HomeComponent],   //Da Importare Se serve "RouterOutlet", "AppProvaComponent"
+  template: `
+  <main>
+    <header class="brand-name">
+    <img class="brand-logo" src="assets/logo.svg" alt="logo" aria-hidden="true" />
+    <br>
+    <br>
+  </header>
+  <section class="content">
+    <app-home></app-home>
+  </section>
+</main>`,
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'ProgettoAnuglarVSC';
